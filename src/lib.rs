c7dae6655629b3a2995c9ef870216d1663b0de74
hatoo <hato2000@gmail.com>
2023-07-08 17:06:10 +0900

    1

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,55 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{
+    collections::HashMap,
+    ops::AddAssign,
+    sync::{Arc, Mutex},
+    time::{Duration, Instant},
+};
+
+use tracing::{span, Subscriber};
+use tracing_subscriber::{registry::LookupSpan, Layer};
+
+pub struct DurationLayer {
+    inner: Arc<Mutex<Inner>>,
+}
+
+struct StartedAt(Instant);
+
+struct Inner {
+    duration: HashMap<&'static str, Duration>,
+}
+
+impl<S> Layer<S> for DurationLayer
+where
+    S: Subscriber,
+    S: for<'lookup> LookupSpan<'lookup>,
+{
+    fn on_new_span(
+        &self,
+        _attrs: &span::Attributes<'_>,
+        id: &span::Id,
+        ctx: tracing_subscriber::layer::Context<'_, S>,
+    ) {
+        let span = ctx.span(id).unwrap();
+
+        span.extensions_mut().insert(StartedAt(Instant::now()));
+    }
+
+    fn on_close(&self, id: span::Id, ctx: tracing_subscriber::layer::Context<'_, S>) {
+        let span = ctx.span(&id).unwrap();
+
+        let name = span.metadata().name();
+        let started_at = span.extensions().get::<StartedAt>().unwrap().0;
+
+        span.extensions_mut().remove::<StartedAt>();
+
+        self.inner
+            .lock()
+            .unwrap()
+            .duration
+            .entry(name)
+            .or_default()
+            .add_assign(started_at.elapsed());
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +57,5 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    fn it_works() {}
 }
